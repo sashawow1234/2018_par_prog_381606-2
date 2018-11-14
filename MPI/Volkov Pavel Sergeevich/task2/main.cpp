d2d7033659c9403766bbe9f92d6ad8d232a25fb2
Snewar79 <himera5x@yandex.ru>
2018-11-15 03:26:30 +0400

    Task 2 v2 complete

--- MPI/Volkov Pavel Sergeevich/task2/main.cpp
+++ MPI/Volkov Pavel Sergeevich/task2/main.cpp
@@ -9,69 +9,116 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <string>
 
-// USING OPENCVV!!!!!!!!!!!!!
 
+
+
+#define N 100
 
 using namespace cv;
 using namespace std;
 
 
-void to_buf(int *buf, Mat mat)
-{
-
-	int length = mat.cols * mat.rows;
-	
-	buf = new int[length];
-
-	int counter = 0;
-
-	for (int i = 0; i < mat.rows; i++)
-		for (int j = 0; j < mat.cols; j++)
-		{
-			buf[counter] = mat.at<cv::Vec3b>(i, j)[0];
-			counter++;
-		}
-
-}
-string name;
-
-Mat image_base;
-
-
-int *buffer;
-int *wh;
-
 int main(int argc, char **argv)
 {
 
-	int * buffer_2;
-	Mat image;
-	int worksize;
 	int taskid;
 	int numtasks;
-	MPI_Status Stat;
-	int st_raw_cout;
+
+	int wh[2];
+
+	int *displacements;
+	int *part_count;
+
+	uchar *image_buffer;
+
+	Mat image;
+	Mat greyMat;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-	if (numtasks == 1)
-	{
-		cout << "Please, get more process" << "\n";
-		return 0;
+	/*
+		//cout << "Task_count = " << numtasks << "\n";
+		int buf[N];
 
-	}
+		
+		
+			int standart_part = N / numtasks;
+
+			int *displacements;
+			int *part_count;
+			part_count = new int[numtasks];
+			displacements = new int[numtasks];
 
 
-	wh = new int[2];
+			for (int i = 0; i < numtasks - 1; i++)
+			{
+				part_count[i] = standart_part;
+				displacements[i] = standart_part * i;
+			}
+			part_count[numtasks - 1] = N - standart_part * (numtasks - 1);
+			displacements[numtasks - 1] = (numtasks - 1) * standart_part;
+
+
+		
+			if (taskid == 0)
+			{
+				for (int i = 0; i < N; i++)
+				{
+					buf[i] = i;
+				}
+
+				for (int i = 0; i < numtasks; i++)
+				{
+					cout << "PT_COUNT " << part_count[i] << " DISP[i] " << displacements[i] << "\n";
+				}
+
+				
+			}
+
+
+
+			int *rec_buf = new int[part_count[taskid]];
+
+
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+
+	
+		MPI_Scatterv(&buf[0], part_count, displacements, MPI_INT, rec_buf, part_count[taskid], MPI_INT, 0, MPI_COMM_WORLD);
+
+
+		string out;
+
+		for (int i = 0; i < part_count[taskid]; i++)
+		{
+			out += std::to_string(rec_buf[i]) + " ";
+		}
+		cout << out << "\n";
+
+
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+
+
+		*/
+
+
+
+
+	//---------------------------------------- Get Image and bcast parametrs widht and heig --------------------/
+
 	if (taskid == 0)
 	{
-
-	
+		
 
 		cout << "Enter the way or name of image: ";
-	
+
+		string name;
+
 		cin >> name;
 
 		image = imread(name, IMREAD_COLOR);
@@ -80,240 +127,166 @@ int main(int argc, char **argv)
 		{
 
 			while (image.empty())
-			cout << "Could not open or find the image\n Try again: " << std::endl;
+			{
+				cout << "Could not open or find the image\n Try again: ";
 
-			cin >> name;
+				cin >> name;
 
-			image = imread(name, IMREAD_COLOR);
+				image = imread(name, IMREAD_COLOR);
+			}
 		}
 
+		
+		cv::cvtColor(image, greyMat, CV_BGR2GRAY);
+		imwrite("Gray_Image_BASE_NEW.jpg", greyMat);
+		wh[0] = image.cols;
+		wh[1] = image.rows;
 
-		buffer = new int[image.rows * image.cols];
-
-		image_base = image.clone();
-
-
+		image_buffer = new uchar[wh[0] * wh[1]];
 
 		int counter = 0;
+
+
 
 		for (int i = 0; i < image.rows; i++)
 			for (int j = 0; j < image.cols; j++)
 			{
-				buffer[counter] = image.at<cv::Vec3b>(i, j)[0] * 0.11 + image.at<cv::Vec3b>(i, j)[1] * 0.59 + image.at<cv::Vec3b>(i, j)[2] * 0.30;
-				image_base.at<cv::Vec3b>(i, j)[0] = image.at<cv::Vec3b>(i, j)[0] * 0.11 + image.at<cv::Vec3b>(i, j)[1] * 0.59 + image.at<cv::Vec3b>(i, j)[2] * 0.30;
-				image_base.at<cv::Vec3b>(i, j)[1] = image.at<cv::Vec3b>(i, j)[0] * 0.11 + image.at<cv::Vec3b>(i, j)[1] * 0.59 + image.at<cv::Vec3b>(i, j)[2] * 0.30;
-				image_base.at<cv::Vec3b>(i, j)[2] = image.at<cv::Vec3b>(i, j)[0] * 0.11 + image.at<cv::Vec3b>(i, j)[1] * 0.59 + image.at<cv::Vec3b>(i, j)[2] * 0.30;
+				//image_buffer[counter] = image.at<cv::Vec3b>(i, j)[0] * 0.11 + image.at<cv::Vec3b>(i, j)[1] * 0.59 + image.at<cv::Vec3b>(i, j)[2] * 0.30;
+				image_buffer[counter] = greyMat.at<uchar>(i, j);
 				counter++;
 			}
 
 
 
-
-
-
-		wh[0] = image.cols;
-		wh[1] = image.rows;
-
-
-		//cout << wh[0] <<  "\n";
-	
-		for (int i = 1; i < numtasks; i++)
-			MPI_Send(&wh[0], 2, MPI_INT, i, 0, MPI_COMM_WORLD);
-		
-		st_raw_cout = wh[1] / (numtasks - 1);
-
-
-		//cout << " st_raw_cout * wh[0] " << st_raw_cout * wh[0] << "\n";
-
-	//	cout << "sta = " << st_raw_cout << "\n";
-		for (int i = 1; i < numtasks - 1; i++)
-		{
-			MPI_Send(&buffer[st_raw_cout * (i - 1)* wh[0]], st_raw_cout * wh[0], MPI_INT, i, 1, MPI_COMM_WORLD);
-		}
-
-	//	cout << "Last " << wh[1] - st_raw_cout * (numtasks - 2) << "\n";
-	//	cout << "last count " << (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0] << "\n";
-
-		MPI_Send(&buffer[image.cols * image.rows - (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0]], (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0], MPI_INT, numtasks - 1, 1, MPI_COMM_WORLD);
-
 	}
 
-	else
+
+
+
+	// All proc getting widh and hei..
+	MPI_Bcast(&wh, 2, MPI_INT, 0, MPI_COMM_WORLD);
+	//cout << "wh[0] " << wh[0] << " ";
+	
+
+	
+
+	//All process calculate data for scatterv
+
+	int standart_part = wh[0] * wh[1] / numtasks;
+
+	part_count = new int[numtasks];
+	displacements = new int[numtasks];
+
+	for (int i = 0; i < numtasks - 1; i++)
 	{
-		int min = 255, max = 0;
-		int gist[256];
-		for (int i = 0; i < 256; i++)
-		{
-			gist[i] = 0;
-		}
+		part_count[i] = standart_part;
+		displacements[i] = standart_part * i;
+	}
+	part_count[numtasks - 1] = wh[0] * wh[1] - standart_part * (numtasks - 1);
+	displacements[numtasks - 1] = (numtasks - 1) * standart_part;
+
+
+	//All proc knows self parameters for scatterv
+
+
+
+
+	uchar *rec_buf = new uchar[part_count[taskid]];
+
+
+	MPI_Scatterv(&image_buffer[0], part_count, displacements, MPI_UNSIGNED_CHAR, rec_buf, part_count[taskid], MPI_INT, 0, MPI_COMM_WORLD);
+
+
+	int min = 256;
+	int max = 0;
+
+	int _min;
+	int _max;
+
+	//cout << "id = " << taskid << " part_count = " << part_count[taskid] << "\n";
+
+	// find min and max intens
+	for (int i = 0; i < part_count[taskid]; i++)
+	{
+		if (rec_buf[i] > max) max = rec_buf[i];
+		if (rec_buf[i] < min) min = rec_buf[i];
+	}
+
+	//cout << "Local min/max = " << min << "/" << max << "\n";
+
+	// Mina and max reduce
+	MPI_Allreduce(&min, &_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+	MPI_Allreduce(&max, &_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 	
 
-		//All
-//		cout  << taskid << "\n";
-		int  *wh_2 = new int[2];
-		MPI_Recv(wh_2, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &Stat);
-	//	cout << "WH[0] = " << wh_2[0] << "\n";
-	//	cout << "WH[1] = " << wh_2[1] << "\n";
-		
-		int st_raw_cout_2 = wh_2[1] / (numtasks - 1);
 
-	//	cout << "st_raw_cout_2 " << st_raw_cout_2 << '\n';
+	//Minimal contrast correction is 5%
+	if (_min < 12) _min = 12;
+	if (_max > 244) _max = 244;
 
-		buffer_2 = new int[st_raw_cout_2 * wh_2[0]];
+	//cout << "All min/max = " << _min << "/" << _max << "\n";
 
 
+	// correction
 
-		if (taskid != numtasks - 1)
-		{
-			MPI_Recv(buffer_2, st_raw_cout_2 * wh_2[0], MPI_INT, 0, 1, MPI_COMM_WORLD, &Stat);
-
-			worksize = st_raw_cout_2 * wh_2[0];
-
-			for (int i = 0; i < st_raw_cout_2 * wh_2[0]; i++)
-			{
-				if (min > buffer_2[i]) min = buffer_2[i];
-				if (max < buffer_2[i]) max = buffer_2[i];
-			}
-
-
-		}
-
-
-		if (taskid == numtasks - 1)
-		{
-			buffer_2 = new int[(wh_2[1] - st_raw_cout_2 * (numtasks - 2)) * wh_2[0]];
-			MPI_Recv(buffer_2, (wh_2[1] - st_raw_cout_2 * (numtasks - 2)) * wh_2[0], MPI_INT, 0, 1, MPI_COMM_WORLD, &Stat);
-
-
-			worksize = (wh_2[1] - st_raw_cout_2 * (numtasks - 2)) * wh_2[0];
-
-			for (int i = 0; i <worksize; i++)
-			{
-				if (min > buffer_2[i]) min = buffer_2[i];
-				if (max < buffer_2[i]) max = buffer_2[i];
-			}
-
-		
-		}
-
-	//	cout << "min = " << min << "\n" << "max = " << max << "\n";;
-		
-		int minmax[2];
-
-		minmax[0] = min;
-		minmax[1] = max;
-
-		MPI_Send(&minmax[0], 2, MPI_INT, 0, 2, MPI_COMM_WORLD);
+	if (_max - _min == 0)
+	{
+		cout << "Image is uniform already!\n";
+		MPI_Finalize();
 	}
+
+	double coef = 255.0 / double(_max - _min);
+
+	for (int i = 0; i < part_count[taskid]; i++)
+	{	
+		int rez;
+		rez = ((int)rec_buf[i] - (int)_min) * coef;
+		
+		rec_buf[i] = rez;
+
+		if (rez > 255) rec_buf[i] = 255;
+		if (rez < 0) rec_buf[i] = 0;
+		
+	}
+
+	uchar *out_image_vector;
 
 	if (taskid == 0)
 	{
-		int true_min = 255, true_max = 0;
-
-		int rec_buf[2];
-
-		for (int i = 1; i < numtasks; i++)
-		{
-			MPI_Recv(rec_buf, 2, MPI_INT, i, 2, MPI_COMM_WORLD, &Stat);
-			if (true_min > rec_buf[0]) true_min = rec_buf[0];
-			if (true_max < rec_buf[1]) true_max = rec_buf[1];
-		}
-		cout << "true min/max " << true_min << "//" << true_max << "\n";
-	
-		int tminmax[2];
-		tminmax[0] = true_min;
-		tminmax[1] = true_max;
-
-
-		for (int i = 1; i < numtasks; i++)
-		{
-			MPI_Send(&tminmax[0], 2, MPI_INT, i, 3, MPI_COMM_WORLD);
-		}
-
-	}
-	else
-	{
-
-
-		int minmax[2];
-		MPI_Recv(minmax, 2, MPI_INT, 0, 3, MPI_COMM_WORLD, &Stat);
-	//	cout << "myrank " << taskid << " tr" << minmax[0] << " " << minmax[1] << "\n";
-
-		double coef = 255.0 / double(minmax[1] - minmax[0]);
-
-		for (int i = 0; i < worksize; i++)
-		{
-			buffer_2[i] = (buffer_2[i] - minmax[0]) * coef;
-			if (buffer_2[i] > 255) buffer_2[i] = 255;
-			if (buffer_2[i] < 0) buffer_2[i] = 0;
-			//cout << buffer_2[i] << " ";
-		}
-
-
-
-		
-			MPI_Send(&buffer_2[0], worksize, MPI_INT, 0, 4, MPI_COMM_WORLD);
-		
-
-
+		out_image_vector = new uchar[wh[0] * wh[1]];
 	}
 
+
+
+	MPI_Gatherv(&rec_buf[0], part_count[taskid], MPI_UNSIGNED_CHAR, &out_image_vector[0], part_count, displacements, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
 
 	if (taskid == 0)
 	{
-		
-		int *last_buf = new int[st_raw_cout * wh[0]];
+		Mat out;
+		out = greyMat.clone();
 
-		Mat out = image.clone();
-		for (int i = 1; i < numtasks - 1; i++)
+		int row;
+		int col;
+
+		for (int i = 0; i < wh[0] * wh[1]; i++)
 		{
-			MPI_Recv(last_buf, st_raw_cout * wh[0], MPI_INT, i, 4, MPI_COMM_WORLD, &Stat);
-
-			for (int j = 0; j < st_raw_cout * wh[0]; j++)
-			{
-				int raw = st_raw_cout * (i - 1) + j / wh[0];
-				int col = j % wh[0];
-				out.at<cv::Vec3b>(raw, col)[0] = last_buf[j];
-				out.at<cv::Vec3b>(raw, col)[1] = last_buf[j];
-				out.at<cv::Vec3b>(raw, col)[2] = last_buf[j];
-				
-			}
-
+			row = i / wh[0];
+			col = i % wh[0];
+			out.at<uchar>(row, col) = out_image_vector[i];
 
 		}
-
 		
-		
-		//cout << "Last " << (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0];
-
-		last_buf = new int[(wh[1] - st_raw_cout * (numtasks - 2)) * wh[0]];
-		MPI_Recv(last_buf, (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0], MPI_INT, numtasks - 1, 4, MPI_COMM_WORLD, &Stat);
-		
-		
-		for (int j = 0; j < (wh[1] - st_raw_cout * (numtasks - 2)) * wh[0]; j++)
-		{
-
-
-			int raw = st_raw_cout * (numtasks - 2) + j / wh[0];
-			int col = j % wh[0];
-		//	cout << "r " << raw << " l " << col << "\n";
-			out.at<cv::Vec3b>(raw, col)[0] = last_buf[j];
-			out.at<cv::Vec3b>(raw, col)[1] = last_buf[j];
-			out.at<cv::Vec3b>(raw, col)[2] = last_buf[j];
-
-		}
-
-
-		imwrite("Gray_Image_contarst_" + name, out);
-		imwrite("Gray_Image_Base_" + name, image_base);
+		imwrite("Contrast_image.jpg", out);
+		//delete out_image_vector;
+		//delete image_buffer;
 
 	}
 
-	// -- ALL
+//	delete part_count;
+//	delete displacements;
 
 
 	MPI_Finalize();
 
-	return 0;
 }
